037aa930ee2fac8cbb76edf9282f65a6d5936be5
Ngo Thi Hong Nhung <n.t.h.nhung@azoom.jp>
2020-04-28 15:48:59 +0700

    :tada: Implement get payment request list api

--- app/routes/applications/payments/get.js
+++ app/routes/applications/payments/get.js
@@ -2,36 +2,53 @@ import { paymentCollection} from '@root/database'
 import getRole from '@helpers/users/getRole.js'
 
 export default async (req, res) => {
-    const { userId } = req.user.id
-    const { page, limit } = req.query
-    const pageNumber = parseInt(page)
-    const limitRow = parseInt(limit)
-    const total = (pageNumber - 1) * limitRow
-    
-    const role = await getRole(userId)
-    if(role == 'admin'){
-        if(pageNumber == 1){
-            const payments =  await paymentCollection().orderBy('created').limit(limitRow).get()
-            if(payments.empty) return res.send([])
-            return res.send(payments.docs.map(doc => doc.data()))
-        }
-        const listPayments =  await paymentCollection().orderBy('created').limit(total).get()
-        if(listPayments.empty) return res.send([])
-        const lastPayment = listPayments.docs[listPayments.docs.length - 1]
-        const payments = await paymentCollection().orderBy('created').startAfter(lastPayment.data().created).limit(limitRow).get()
-        return res.send(payments.docs.map(doc => doc.data()))
-    }else if(role == 'user'){
-        if(pageNumber == 1){
-            const payments = await paymentCollection().where('userId', '==', userId).orderBy('created').limit(limitRow).get()
-            if(payments.empty) return res.send([])
-            return res.send(payments.docs.map(doc => doc.data()))
-        }
-        const listPayments =  await paymentCollection().where('userId', '==', userId).orderBy('created').limit(total).get()
-        if(listPayments.empty) return res.send([])
-        const lastPayment = listPayments.docs[listPayments.docs.length - 1]
-        const payments = await paymentCollection().where('userId', '==', userId).orderBy('created').startAfter(lastPayment.data().created).limit(limitRow).get()
-        return res.send(payments.docs.map(doc => doc.data()))
-    }else{
-        return res.sendStatus(403)
+  const { userId } = req.user.id
+  let { page, limit } = req.query
+
+  // TODO: page, limit must be greater than 0  (handle be OpenAPI)
+  // TODO: remove 2 line parser below when openAPI is applied
+  page = parseInt(page)
+  limit = parseInt(limit)
+  const total = (page - 1) * limit
+  
+  const role = await getRole(userId)
+  if (role == 'admin') {
+    let connection = paymentCollection().orderBy('created')
+    const getAllPayments = await connection.get()
+    const count = getAllPayments.docs.length
+    if (page === 1) {
+      const payments =  await connection.limit(limit).get()
+      if(payments.empty) return res.send({'count': count, 'data': []})
+      return res.send({
+        'count': count, 
+        'data': payments.docs.map(doc => doc.data())
+      })
     }
+    const lastPayment = getAllPayments.docs[total - 1]
+    const payments = await connection.startAfter(lastPayment.data().created).limit(limit).get()
+    return res.send({
+      'count': count, 
+      'data': payments.docs.map(doc => doc.data())
+    })
+  } else if (role == 'user') {
+    let connection = paymentCollection().where('userId', '==', userId).orderBy('created')
+    const getAllPayments = await connection.get()
+    const count = getAllPayments.docs.length
+    if (page === 1) {
+      const payments = await connection.limit(limit).get()
+      if(payments.empty) return res.send({'count': count, 'data': []})
+      return res.send({
+        'count': count, 
+        'data': payments.docs.map(doc => doc.data())
+      })
+    }
+    const lastPayment = getAllPayments.docs[total - 1]
+    const payments = await connection.startAfter(lastPayment.data().created).limit(limit).get()
+    return res.send({
+      'count': count, 
+      'data': payments.docs.map(doc => doc.data())
+    })
+  } else {
+    return res.sendStatus(403)
+  }
 }
