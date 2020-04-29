7e7d4a1427bb42b7d6d1d0320a685a8e2146ca07
Ngo Thi Hong Nhung <n.t.h.nhung@azoom.jp>
2020-04-29 10:44:48 +0700

    get payment request list api

--- app/routes/applications/payments/get.js
+++ app/routes/applications/payments/get.js
@@ -2,41 +2,37 @@ import { paymentCollection} from '@root/database'
 import getRole from '@helpers/users/getRole.js'
 
 export default async (req, res) => {
-  const { userId } = req.user.id
+  const userId = req.user.id
   let { page, limit } = req.query
 
   // TODO: page, limit must be greater than 0  (handle be OpenAPI)
   // TODO: remove 2 line parser below when openAPI is applied
   page = parseInt(page)
   limit = parseInt(limit)
-  const total = (page - 1) * limit
+  const totalIgnorePayment = (page - 1) * limit
 
   const role = await getRole(userId)
   if(role !== 'user' && role !== 'admin') return res.sendStatus(403) 
 
-  let connectionGetAll = paymentCollection().orderBy('created')
-  let connection = paymentCollection().orderBy('created').limit(limit)
-
+  let connection = paymentCollection()
   if (role == 'user') {
-    connectionGetAll = connectionGetAll.where('userId', '==', userId)
     connection = connection.where('userId', '==', userId)
   }
-  const getAllPayments = await connectionGetAll.get()
-  const count = getAllPayments.docs.length
-  
+
+  const allPayments = await connection.orderBy('created').get()
+  connection = connection.orderBy('created').limit(limit)
+
+  let payments
   if (page === 1) {
-    const payments =  await connection.get()
-    if(payments.empty) return res.send({ count, 'data': []})
-    return res.send({
-      count, 
-      'data': payments.docs.map(doc => doc.data())
-    })
+    payments =  await connection.get()
+    if(payments.empty) return res.send({ 'count' : allPayments.size, 'data': []})
+  } else {
+    const lastIgnorePayment = allPayments.docs[totalIgnorePayment - 1]
+    payments = await connection.startAfter(lastIgnorePayment.data().created).get()
   }
 
-  const lastPayment = getAllPayments.docs[total - 1]
-  const payments = await connection.startAfter(lastPayment.data().created).get()
   return res.send({
-    count, 
+    'count' : allPayments.size, 
     'data': payments.docs.map(doc => doc.data())
   })
 }
