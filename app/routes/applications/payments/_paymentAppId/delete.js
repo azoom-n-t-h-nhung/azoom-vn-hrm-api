0a18c455972016ef14b36d0ef989a40088724cdf
Ngo Thi Hong Nhung <n.t.h.nhung@azoom.jp>
2020-05-07 13:14:53 +0700

    delete payment api

--- app/routes/applications/payments/_paymentAppId/delete.js
+++ app/routes/applications/payments/_paymentAppId/delete.js
@@ -3,17 +3,15 @@ import getRole from '@helpers/users/getRole.js'
 import { status } from '@constants/index.js'
 
 export default async (req, res) => {
-    const userId = req.user.id
-    const { paymentAppId } = req.query
+  const userId = req.user.id
+  const { paymentAppId } = req.query
 
-    const payment = await paymentCollection().doc(paymentAppId).get()
-    if( !payment.exists ) return res.sendStatus(404)
-    if( payment.data().status !== status.inPending ) return res.sendStatus(400)
+  const payment = await paymentCollection().doc(paymentAppId).get()
+  if (!payment.exists) return res.sendStatus(404)
+  if (payment.data().status !== status.inPending) return res.sendStatus(400)
 
-    const role = await getRole(userId)
-    if( role == 'admin' || role == 'editor' || ( role == 'user' && payment.data().userId == userId ) ){
-        await paymentCollection().doc(paymentAppId).update({ isActive: false })
-        return res.sendStatus(200)
-    }
-    return res.sendStatus(403)
+  const role = await getRole(userId)
+  if (role != 'admin' && payment.data().userId !== userId) return res.sendStatus(403)
+  await paymentCollection().doc(paymentAppId).update({ isActive: false })
+  return res.sendStatus(200)
 }
